55276f1990835f0c4b2b0e9ddd1f028b51a2f41a
cynthia-jq12 <cxnthiazen12@gmail.com>
2026-04-23 10:42:05 +0700

    Add Boucy material and update SampleScene
    Add a new 2D physics material 'Boucy' (high bounciness) and SceneTemplateSettings. Substantially update Assets/Scenes/SampleScene.unity: bump serialized versions, add multiple GameObjects (sprites, colliders, rigidbodies, buoyancy effector for water), animator/controller references and transform data for platformer level geometry. Update several sprite animation/controller assets and modify Player2DController.cs and ProjectSettings/TagManager.asset to match the scene changes.

--- Assets/Scripts/Player2DController.cs
+++ Assets/Scripts/Player2DController.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player2DController : MonoBehaviour
@@ -8,6 +8,7 @@ public class Player2DController : MonoBehaviour
     
     private Rigidbody2D _rb;
     private float moveInputValue;
+    private bool _isGrounded;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,13 +21,24 @@ public class Player2DController : MonoBehaviour
     {
         if (Keyboard.current != null)
         {
-            moveInputValue = (Keyboard.current.dKey.isPressed ? 1 : 0) - (Keyboard.current.aKey.isPressed ? 1 : 0);
+            moveInputValue = (Keyboard.current.dKey.isPressed ? 1 : 0) - (Keyboard.current.aKey.isPressed ? 1 : 0); // กด A or D เพื่อเดิน
         }
-        _rb.linearVelocity = new Vector2(moveInputValue * speed, _rb.linearVelocity.y);
+        _rb.linearVelocity = new Vector2(moveInputValue * speed, _rb.linearVelocity.y);// ใส่เเรงแกน Y คงแรงแกน X
 
-        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && _isGrounded) // กด space bar พร้อมติดพื้น
         {
-            _rb.AddForce(new Vector2(_rb.linearVelocity.x, jumpForce));
+            _rb.AddForce(new Vector2(_rb.linearVelocity.x, jumpForce)); // ใส่เเรงแกน Y คงแรงแกน X
         }
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Groud"))
+        {
+            _isGrounded = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        _isGrounded = false;
     }
 }
